3e51278ec7a4fedbeca8239225d949db6c03a793
Rachelle Hu <rachelle.hu123@gmail.com>
2021-07-09 19:49:23 -0400

    Update gmaps.py

--- gmaps.py
+++ gmaps.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 import googlemaps
 
-gmaps = googlemaps.Client(key='AIzaSyBx2lGCeaLjMTNblROj3I4iNL8DWi45jvk')
 
-address = "6392 Truckee Court, Newark, CA"  # should actually come from input
-now = datetime.now()
-directions_result = gmaps.directions(address, "fire station near "+address,
-                                     mode="driving",
-                                     departure_time=now)
+def time_estimate(address="6392 Truckee Court, Newark, CA"):
+    gmaps = googlemaps.Client(key='AIzaSyBx2lGCeaLjMTNblROj3I4iNL8DWi45jvk')
 
-time = directions_result[0]['legs'][0]['duration']['text']
-destination_address = directions_result[0]['legs'][0]['end_address']
-print(time)
-print(destination_address)
+    now = datetime.now()
+    directions_result = gmaps.directions(address, "fire station near "+address,
+                                         mode="driving",
+                                         departure_time=now)
+
+    time = directions_result[0]['legs'][0]['duration']['text']
+    destination_address = directions_result[0]['legs'][0]['end_address']
+    return time, destination_address
