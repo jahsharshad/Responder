ab5f09589cdf9e47768019e7cc1c5b851e17bbd6
jollypolly123 <rachelle.hu123@gmail.com>
2021-07-10 11:18:29 -0400

    gmaps function

--- gmaps.py
+++ gmaps.py
@@ -7,7 +7,8 @@ api_key = 'AIzaSyBx2lGCeaLjMTNblROj3I4iNL8DWi45jvk'
 
 def generateMap(address):
     # "3749 Armour Court, Fremont, CA 94555 USA"
-    # input type, connect gmaps py to html and call function while giving input for address, making input for google maps on page
+    # input type, connect gmaps py to html and call function while giving input for address,
+    # making input for google maps on page
     gmaps = googlemaps.Client(key=api_key)
     # example address = "6392 Truckee Court, Newark, CA"
     now = datetime.now()
@@ -18,11 +19,11 @@ def generateMap(address):
     clipped_address = " ".join(str(i) for i in clipped_address)
 
     directions_result1 = gmaps.directions(clipped_address, "fire station near "+clipped_address,
-                                        mode="driving",
-                                        departure_time=now)
+                                          mode="driving",
+                                          departure_time=now)
     directions_result2 = gmaps.directions(clipped_address, "hospital near "+clipped_address,
-                                        mode="driving",
-                                        departure_time=now)
+                                          mode="driving",
+                                          departure_time=now)
 
     timeString1 = directions_result1[0]['legs'][0]['duration']['text']
     timeString2 = directions_result2[0]['legs'][0]['duration']['text']
@@ -43,7 +44,7 @@ def generateMap(address):
     print("Address:", address)
 
     destination = destination_address.replace(",", "")
-    destination = destination_address.split(" ")
+    destination = destination.split(" ")
 
     address = address.replace(",", "")
     address = address.split(" ")
@@ -56,34 +57,6 @@ def generateMap(address):
 
 
 # print(generateMap("3749 Armour Court, Fremont, CA 94555 USA"))
-
-
-def time_estimate(address="6392 Truckee Court, Newark, CA"):
-    gmaps = googlemaps.Client(key='AIzaSyBx2lGCeaLjMTNblROj3I4iNL8DWi45jvk')
-
-    now = datetime.now()
-    directions_result1 = gmaps.directions(address, "fire station near "+address,
-                                          mode="driving",
-                                          departure_time=now)
-    directions_result2 = gmaps.directions(address, "hospital near "+address,
-                                          mode="driving",
-                                          departure_time=now)
-
-    timeString1 = directions_result1[0]['legs'][0]['duration']['text']
-    timeString2 = directions_result2[0]['legs'][0]['duration']['text']
-
-    time1 = int(timeString1.split()[0])
-    time2 = int(timeString2.split()[0])
-
-    if time1 < time2:
-        time = timeString1
-        distance = directions_result1[0]['legs'][0]['distance']['text']
-        destination_address = directions_result1[0]['legs'][0]['end_address']
-    else:
-        time = timeString2
-        distance = directions_result1[0]['legs'][0]['distance']['text']
-        destination_address = directions_result1[0]['legs'][0]['end_address']
-    return time, destination_address, distance
 
 '''
 Reference"
