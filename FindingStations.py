9d54ba0162fe7a2e4fd7c53c36104d3755eb5924
Jahsh Arshad <86214979+jahsharshad@users.noreply.github.com>
2021-07-10 15:21:40 -0700

    Update FindingStations.py

--- FindingStations.py
+++ FindingStations.py
@@ -46,7 +46,9 @@ def stationCalc(county, state):
                 stationAddresses.append(y[i]['formatted_address'])
 
     county_station = set(county_station)
-    return len(county_station), stationAddresses
+    stationNumber = len(county_station)
+    county_station = list(county_station)
+    return stationNumber, stationAddresses, county_station
 
 
 county_hospital = []
@@ -83,16 +85,13 @@ def hospitalCalc(county, state):
             # Append value corresponding to the
             # 'name' key at the ith index of y
             hospitalName = y[i]['name']
-            if ('Pet' not in hospitalName) and ('Vet' not in hospitalName) and ('Animal' not in hospitalName):
-                if ('Cat' not in hospitalName) and ('Dog' not in hospitalName):
-                    if ('Hospital' in hospitalName) or ('Center' in hospitalName):
-                        county_hospital.append(y[i]['name'])
-                        hospitalAddresses.append(y[i]['formatted_address'])
+            if ('Pet' not in hospitalName) and ('Vet' not in hospitalName) and ('Animal' not in hospitalName)\
+            and ('Cat' not in hospitalName) and ('Dog' not in hospitalName):
+                if ('Hospital' in hospitalName):
+                    county_hospital.append(y[i]['name'])
+                    hospitalAddresses.append(y[i]['formatted_address'])
 
     county_hospital = set(county_hospital)
     hospitalNumber = len(county_hospital)
-    return hospitalNumber, hospitalAddresses
-
-AlamedaStationNum, AlamedaStationAddresses = stationCalc('Alameda','CA')
-AlamedaHospitalNum, AlamedaHospAddresses = hospitalCalc('Alameda','CA')
-
+    county_hospital = list(county_hospital)
+    return hospitalNumber, hospitalAddresses, county_hospital
