7295e54980e28d5d92bd4dd1c98734e5841798b0
Rachelle Hu <rachelle.hu123@gmail.com>
2021-07-09 22:18:30 -0400

    Update gmaps.py

--- gmaps.py
+++ gmaps.py
@@ -32,3 +32,17 @@ else:
 print(time)
 print(distance)
 print(destination_address)
+
+
+def time_estimate(address="6392 Truckee Court, Newark, CA"):
+    gmaps = googlemaps.Client(key='AIzaSyBx2lGCeaLjMTNblROj3I4iNL8DWi45jvk')
+
+    now = datetime.now()
+    directions_result = gmaps.directions(address, "fire station near "+address,
+                                         mode="driving",
+                                         departure_time=now)
+
+    time = directions_result[0]['legs'][0]['duration']['text']
+    destination_address = directions_result[0]['legs'][0]['end_address']
+    return time, destination_address
+
