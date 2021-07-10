10b61120c6c7606c13ce6b433094c76cc639c4e2
Anuj Naik <41352184+AnujNaik@users.noreply.github.com>
2021-07-09 17:07:13 -0700

    ML Model Done
    The model is done and can be run with:
    import model
    model.predict(stations, area, urban)

---
+++ model.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+
+def train_model():
+    data = pd.read_csv("ResponseTimes.csv")
+
+    X = data[["EmergencyStations", "Area", "Urban"]]
+    Y = data[["ResponseTime"]]
+
+    Y = Y.to_numpy()
+    Y = Y.flatten()
+
+    clf = RandomForestRegressor()
+    clf.fit(X, Y)
+
+    predicted = clf.predict(X)
+
+
+    avg=0
+    for i in range(len(predicted)-1):
+        avg = avg + abs(predicted[i] - Y[[i]])
+        print(avg)
+
+    print("Loss: ", avg/len(predicted))
+
+
+    ans = input("Save?")
+    if ans == "y":
+        filename = 'model.sav'
+        joblib.dump(clf, "./random_forest.joblib")
+
+def predict(stations, area, urban):
+    clf = joblib.load("./random_forest.joblib")
+    prediction = clf.predict([[stations, area, urban]])
+    prediction = prediction.flatten()
+    return prediction[0].item()
+
+print(predict(429,789,0.5))
+print(type(predict(429,789,0.5)))
