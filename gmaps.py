3a53792bfccbe0c76bfa77a75eddd36a4766e7de
Jahsh Arshad <86214979+jahsharshad@users.noreply.github.com>
2021-07-09 17:45:19 -0700

    Update gmaps.py
    Added the distance, as well as updating the code to show the fastest time to a fire station or a hospital (instead of just a fire station)

--- gmaps.py
+++ gmaps.py
@@ -1,15 +1,34 @@
 from datetime import datetime
 import googlemaps
 
+gmaps = googlemaps.Client(key='AIzaSyBx2lGCeaLjMTNblROj3I4iNL8DWi45jvk')
 
-def time_estimate(address="6392 Truckee Court, Newark, CA"):
-    gmaps = googlemaps.Client(key='AIzaSyBx2lGCeaLjMTNblROj3I4iNL8DWi45jvk')
+address = "6392 Truckee Court, Newark, CA"  # should actually come from input
+now = datetime.now()
+directions_result1 = gmaps.directions(address, "fire station near "+address,
+                                     mode="driving",
+                                     departure_time=now)
+directions_result2 = gmaps.directions(address, "hospital near "+address,
+                                     mode="driving",
+                                     departure_time=now)
 
-    now = datetime.now()
-    directions_result = gmaps.directions(address, "fire station near "+address,
-                                         mode="driving",
-                                         departure_time=now)
 
-    time = directions_result[0]['legs'][0]['duration']['text']
-    destination_address = directions_result[0]['legs'][0]['end_address']
-    return time, destination_address
+timeString1 = directions_result1[0]['legs'][0]['duration']['text']
+timeString2 = directions_result2[0]['legs'][0]['duration']['text']
+
+
+time1 = int(timeString1.split()[0])
+time2 = int(timeString2.split()[0])
+
+if time1 < time2:
+    time = timeString1
+    distance = directions_result1[0]['legs'][0]['distance']['text']
+    destination_address = directions_result1[0]['legs'][0]['end_address']
+else:
+    time = timeString2
+    distance = directions_result1[0]['legs'][0]['distance']['text']
+    destination_address = directions_result1[0]['legs'][0]['end_address']
+
+print(time)
+print(distance)
+print(destination_address)
